a5a42cd2f8b0c2aa0382ad465cf0d16c9bf0b891
s1lkier <zhumabaevtop@gmail.com>
2025-08-12 19:14:58 +0300

    Кактус

--- scripts/skins.gd
+++ scripts/skins.gd
@@ -4,5 +4,6 @@ extends Node
 enum {VALLEY, DESERT}
 var sprites_backgrounds: Array[CompressedTexture2D] = [preload("res://assets/background1.png"), preload("res://assets/desertBackground.png")]
 var sprites_spawner: Array[CompressedTexture2D] = [preload("res://assets/appleTree1.png"), preload("res://assets/RollField.png")]
+var sprites_drop: Array[CompressedTexture2D] = [preload("res://assets/pixelApple1.png"), preload("res://assets/cactus.png")]
 
 var choice = 0
---
+++ scripts/texture_rect.gd
@@ -0,0 +1,3 @@
+extends TextureRect
+func _ready() -> void:
+	texture = Skins.sprites_drop[Skins.choice]
