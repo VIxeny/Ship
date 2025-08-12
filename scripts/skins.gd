41e97b89fe90594b61abf94b4f552a98911528b0
Vixen <Basov977@gmail.com>
2025-08-12 16:35:41 +0300

    Preparation for skins

---
+++ scripts/skins.gd
@@ -0,0 +1,7 @@
+extends Node
+
+
+enum {APPLE, CACTUS}
+var sprites_objects: Array[CompressedTexture2D] = [preload("res://assets/pixelApple1.png"), preload("res://assets/RollField.png")]
+
+var choice = 0
